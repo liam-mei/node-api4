eff0a4b792ba4b69dc147fd01b2b78f2f6d876eb
Derrick Mei <derrick.mei.168@gmail.com>
2019-12-13 02:10:25 -0600

    changed index to use env vars

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 // code away!
+require("dotenv").config();
+
 const server = require("./server");
 
-const port = 8080;
+const port = process.env.PORT || 8080;
+
 server.listen(port, () => {
   console.log(`\n*** Listening on port ${port} ***\n`);
 });
